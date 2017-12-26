78ab8f9d47386c6886eea0f5a2f12705479d5104
Micky774 <34613774+Micky774@users.noreply.github.com>
2017-12-26 17:18:39 -0500

    Add files via upload

---
+++ IFSC/src/IFSDecoder.java
@@ -0,0 +1,95 @@
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.util.Random;
+
+import javax.swing.JFrame;
+import javax.swing.Timer;
+
+public class IFSDecoder {
+
+	private static BufferedImage[] canvas = new BufferedImage[2];
+	private static final int HEIGHT = 1200;
+	private static final int WIDTH = 1200;
+	private static final int rangeBlockSize = 8;
+	private static final int rangeBlockCount = HEIGHT * WIDTH / (rangeBlockSize * rangeBlockSize);
+	private static int delay = 0;
+	private static int iterativeCount = 0;
+	private static int iterativeMax = 100;
+	private static int tag = 0;
+	private static Random rand = new Random();
+	private static CMap[] functionList = new CMap[rangeBlockCount];
+	Timer timer = new Timer(0, event -> IFSDecoder.run());
+	static JFrame WINDOW = new JFrame() {
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+
+		@Override
+		public void paint(java.awt.Graphics g) {
+			g.drawImage(canvas[0], 0, 0, null);
+		}
+
+	};
+
+	private static void domainInitialize() {
+		for (int i = 0; i < HEIGHT; i++) {
+			for (int j = 0; j < WIDTH; j++) {
+				byte k = (byte) (rand.nextDouble() * 255);
+				int g = k;
+				g = (g << 8) + k;
+				g = (g << 8) + k;
+				canvas[0].setRGB(i, j, g);
+			}
+		}
+
+	}
+
+	private static void domainMap() {
+		for (int r = 0; r < HEIGHT / rangeBlockSize; r++) {
+			for (int s = 0; s < WIDTH / rangeBlockSize; s++) {
+				functionList[r * WIDTH / rangeBlockSize + s].map(canvas[tag], canvas[(tag + 1) % 2], s * rangeBlockSize,
+						r * rangeBlockSize, rangeBlockSize);
+				;
+			}
+
+		}
+		tag = (tag + 1) % 2;
+	}
+
+	private static void updateScreen() {
+		domainMap();
+	}
+
+	private static void run() {
+		if (iterativeCount < iterativeMax) {
+			updateScreen();
+			WINDOW.repaint();
+			iterativeCount++;
+		}
+
+	}
+
+	public IFSDecoder() {
+		canvas[0] = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+		canvas[1] = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+		WINDOW.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		WINDOW.setSize(WIDTH, HEIGHT);
+		domainInitialize();
+		WINDOW.repaint();
+		WINDOW.setVisible(true);
+		timer.setInitialDelay(delay);
+		timer.setDelay(delay);
+		timer.start();
+
+	}
+
+	public IFSDecoder(File f) {
+
+	}
+
+	public static void main(String[] args) {
+		new IFSDecoder();
+	}
+
+}
---
+++ IFSC/src/PTT.java
@@ -0,0 +1,137 @@
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+import javax.imageio.ImageIO;
+
+public class PTT {
+
+	@SuppressWarnings("static-access")
+	public static void write(final String inputFile, final String outputFile) {
+		BufferedImage image;
+		try {
+			// Create streams/initiate files
+			File imageFile = new File(inputFile);
+			image = ImageIO.read(imageFile);
+			OutputStream outputStream = new FileOutputStream(outputFile);
+			int HEIGHT = image.getHeight();
+			int WIDTH = image.getWidth();
+			System.out.println(WIDTH);
+			System.out.println(HEIGHT);
+			byte[] HW = new byte[8];
+			// ByteBuffer buffer = ByteBuffer.allocate(8);
+			ByteBuffer buffer = ByteBuffer.wrap(HW);
+			buffer.order(ByteOrder.BIG_ENDIAN);
+			buffer.putInt(HEIGHT);
+			buffer.putInt(WIDTH);
+			buffer.flip();
+			// System.out.println(new String(HW));
+			outputStream.write(buffer.array());
+			// PTT.printBA(buffer.array());
+			PTT.printBA(HW);
+
+			// create a byte array to load RGB int and wrap with ByteBuffer
+			byte[] rgb = new byte[4];
+			ByteBuffer bb = ByteBuffer.wrap(rgb);
+			bb.order(ByteOrder.BIG_ENDIAN);
+			// iterate thru image assigning RBB ints->byte[4]->Stream(0-2)
+			for (int i = 0; i < HEIGHT; i++) {
+				for (int j = 0; j < WIDTH; j++) {
+					int irgb = image.getRGB(j, i);
+					rgb = bb.allocate(4).putInt(irgb).array();
+					outputStream.write(rgb, 1, 3);
+					// PTT.printBA(rgb);
+				}
+			}
+			outputStream.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@SuppressWarnings("static-access")
+	public static void write(BufferedImage img, final String outputFile) {
+		BufferedImage image = img;
+		try {
+			FileOutputStream outputStream = new FileOutputStream(outputFile);
+			int HEIGHT = image.getHeight();
+			int WIDTH = image.getWidth();
+			ByteBuffer buffer = ByteBuffer.allocate(8);
+			buffer.order(ByteOrder.BIG_ENDIAN);
+			buffer.putInt(HEIGHT);
+			buffer.putInt(WIDTH);
+			buffer.flip();
+			// System.out.println(new String(HW));
+			outputStream.write(buffer.array());
+			System.out.println(new String(buffer.array()));
+			// create a byte array to load RGB int and wrap with ByteBuffer
+			byte[] rgb = new byte[4];
+			ByteBuffer bb = ByteBuffer.wrap(rgb);
+			bb.order(ByteOrder.BIG_ENDIAN);
+			// iterate thru image assigning RBB ints->byte[4]->Stream(0-2)
+			for (int i = 0; i < HEIGHT; i++) {
+				for (int j = 0; j < WIDTH; j++) {
+					int irgb = image.getRGB(i, j);
+					bb.putInt(irgb);
+					outputStream.write(rgb, 1, 3);
+				}
+			}
+			outputStream.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Transcribes a given int value to a byte array
+	 * 
+	 * @param Int
+	 *            The given integer needed to be transcribed
+	 * @param size
+	 *            How many bytes of the integer to be transcribed. 0<=size<=4
+	 * @param LTR
+	 *            Boolean determining transcription order: 1=Left to Right, 0=Right
+	 *            to Left
+	 * @return A byte[] array containing the individual bytes of the given integer
+	 *         in the given order.
+	 */
+	public static byte[] transcribe(int Int, int size, boolean LTR) {
+		byte[] result = new byte[size];
+		int i = 0;
+		if (!LTR) {
+			while (i != size) {
+				result[i] = (byte) (Int & 0xFF);
+				Int >>= 8;
+				i++;
+			}
+		} else {
+			while (i != size) {
+				result[size - i - 1] = (byte) (Int & 0xFF);
+				Int >>= 8;
+				i++;
+			}
+		}
+		return result;
+
+	}
+
+	public static void printBA(byte[] ba) {
+		for (byte b : ba) {
+
+			System.out.println(String.format("%8s", Integer.toBinaryString(b & 0xFF)).replace(' ', '0'));
+		}
+	}
+
+	public static void main(String[] args) {
+		// PTT.printBA(PTT.transcribe(17, 1, false));
+
+		PTT.write("Dice.png", "DiceTest");
+		System.out.println("Success!");
+
+	}
+
+}
