6ba928e60b197a095d2f8972aa41a6810319b752
micky774 <zainmeekail@gmail.com>
2017-12-31 17:23:05 -0500

    Please work

---
+++ IFSC/src/CMap.java
@@ -0,0 +1,27 @@
+import java.awt.image.BufferedImage;
+import java.nio.ByteBuffer;
+
+public class CMap {
+	public int size;
+	public int config;
+	public short contrast;
+	public short brightness;
+
+	public void map(BufferedImage domain, BufferedImage range, int xStart, int yStart, int size) {
+
+	}
+
+	public CMap(byte[] code, int _size) {
+		ByteBuffer bb = ByteBuffer.wrap(code);
+		size = _size;
+		brightness = (short) TTP.byteToInt(bb.get());
+		short temp = (short) TTP.byteToInt(bb.get());
+		config = temp >> 2;
+		contrast = (temp);
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
--- src/IFSEncoder.java
+++
@@ -1,9 +0,0 @@
-
-public class IFSEncoder {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
