276c6e189dc017e4f881e4ca1dba8697ae78f993
micky774 <zainmeekail@gmail.com>
2018-01-01 02:27:14 -0500

    lsave

--- IFSC/src/CMap.java
+++ IFSC/src/CMap.java
@@ -6,6 +6,7 @@ public class CMap {
 	public int config;
 	public short contrast;
 	public short brightness;
+	public int position;
 
 	public void map(BufferedImage domain, BufferedImage range, int xStart, int yStart, int size) {
 
@@ -16,8 +17,18 @@ public class CMap {
 		size = _size;
 		brightness = (short) TTP.byteToInt(bb.get());
 		short temp = (short) TTP.byteToInt(bb.get());
-		config = temp >> 2;
-		contrast = (temp);
+		config = temp >> 5;
+		contrast = (short) (temp & 0x31);
+		temp = (short) TTP.byteToInt(bb.get());
+		contrast += (short) (temp >> 3);
+		position = (short) (temp & 0x7);
+		temp = (short) TTP.byteToInt(bb.get());
+		position += (short) temp;
+		temp = (short) TTP.byteToInt(bb.get());
+		position += (short) temp;
+		temp = (short) TTP.byteToInt(bb.get());
+		position += (short) temp;
+
 	}
 
 	public static void main(String[] args) {
