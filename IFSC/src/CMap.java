650b5103ecb9361112c3db30c1638ea10079514f
micky774 <zainmeekail@gmail.com>
2017-12-31 21:27:49 -0500

    Saving

--- IFSC/src/CMap.java
+++ IFSC/src/CMap.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import java.awt.image.BufferedImage;
 import java.nio.ByteBuffer;
 
@@ -28,32 +27,3 @@ public class CMap {
 	}
 
 }
-=======
-import java.awt.image.BufferedImage;
-import java.nio.ByteBuffer;
-
-public class CMap {
-	public int size;
-	public int config;
-	public short contrast;
-	public short brightness;
-
-	public void map(BufferedImage domain, BufferedImage range, int xStart, int yStart, int size) {
-
-	}
-
-	public CMap(byte[] code, int _size) {
-		ByteBuffer bb = ByteBuffer.wrap(code);
-		size = _size;
-		brightness = (short) TTP.byteToInt(bb.get());
-		short temp = (short) TTP.byteToInt(bb.get());
-		config = temp >> 2;
-		contrast = (temp);
-	}
-
-	public static void main(String[] args) {
-
-	}
-
-}
->>>>>>> branch 'master' of https://github.com/Micky774/FCompression.git
